52c2d293c35af9fa400f984702c20211cccb0d48
victoriaaak <victoria.kalabali@gmail.com>
2025-04-04 21:41:15 +0300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//Viktoria Kalabali
+//I slowed the pace of the creation of the circles with frameRate
+function setup() {
+ canvas.parent('ass1p') 
+ let canvas = createCanvas(400, 400);
+  background(100);
+  frameRate(5);
+
+}
+//I set random x,y for the circles
+function draw() {
+let x=random(0,400);
+let y=random(0,400);
+//i removed the filling of the circles
+noFill();
+// i set random grey colors for the circles including white and black
+stroke(random(0,255))
+ circle(x,y,100);
+}
+//Function to stop the programm
+function doubleClicked(){
+  noLoop()
+}
+//Function to restart it 
+function mousePressed(){
+  loop()
+}
